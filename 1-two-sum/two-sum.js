63d3fab21e9ab89eeed3f4b817695bc7e2648593
Nicholas Orsi <138147970+nickorsi@users.noreply.github.com>
2024-03-27 11:52:03 -0700

    Time: 52 ms (90.49%) | Memory: 51.9 MB (14.75%) - LeetSync

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    // Can sort the array, which has O(n*logn) time complexity
+    const sorted = nums.toSorted((a,b) => (a-b));
+    // Once sorted can do two pointers to find if integers add up to target
+    let left = 0;
+    let right = nums.length-1;
+    while(left < right) {
+        // If equal
+        if(sorted[left] + sorted[right] === target) {
+        // Find index of first value
+            const firstInd = nums.indexOf(sorted[left]);
+        // Find last index of second value
+            const secondInd = nums.lastIndexOf(sorted[right]);
+        // Return array of indeces
+            return [firstInd, secondInd];
+        }
+        // If greater, move right down
+        if(sorted[left] + sorted[right] > target) {
+            right--;
+            continue;
+        }
+        // If less, move left up
+        else {
+            left++;
+            continue;
+        }
+    }
+};
