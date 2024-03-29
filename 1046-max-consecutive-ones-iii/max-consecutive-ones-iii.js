42dd496ac47eaad012ab38b789ad5f5a8ff9d5dc
Nicholas Orsi <138147970+nickorsi@users.noreply.github.com>
2024-03-29 14:34:55 -0700

    Time: 66 ms (48.67%) | Memory: 55.2 MB (10.81%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.js
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.js
@@ -4,32 +4,52 @@
  * @return {number}
  */
 var longestOnes = function(nums, k) {
-//     Can contain k zeros in the array
-//     Declare length = 0
-    let currLength = 0;
-//     Declare maxLength = currLength;
-    let maxLength = currLength;
-//     Declare windowStart = 0
+// //     Can contain k zeros in the array
+// //     Declare length = 0
+//     let currLength = 0;
+// //     Declare maxLength = currLength;
+//     let maxLength = currLength;
+// //     Declare windowStart = 0
+//     let windowStart = 0;
+// //     Decalre zeroCount = 0
+//     let zeroCount = 0;
+// //     For num of nums loop
+//     for(const num of nums) {
+// //      increment length
+//         currLength++;
+// //      if num = 0 increment zeroCount
+//         if (num === 0) zeroCount++;
+// //      while zeroCount > k
+//         while (zeroCount > k) {
+// //          if nums[windowStart] is 0, decrement zeroCount
+//             if (nums[windowStart] === 0) zeroCount--;
+// //          decrement length
+//             currLength--;
+// //          increment windowStart
+//             windowStart++;
+//         }
+//         maxLength = Math.max(maxLength, currLength);
+//     }
+// //     Return maxLength
+//     return maxLength;
+//  Alt Solution
+//  Same as above BUT max length can be established from defined variables
+//    Declare windowStart = 0
     let windowStart = 0;
 //     Decalre zeroCount = 0
     let zeroCount = 0;
 //     For num of nums loop
     for(const num of nums) {
-//      increment length
-        currLength++;
 //      if num = 0 increment zeroCount
         if (num === 0) zeroCount++;
 //      while zeroCount > k
-        while (zeroCount > k) {
+        if (zeroCount > k) {
 //          if nums[windowStart] is 0, decrement zeroCount
             if (nums[windowStart] === 0) zeroCount--;
-//          decrement length
-            currLength--;
 //          increment windowStart
             windowStart++;
         }
-        maxLength = Math.max(maxLength, currLength);
     }
-//     Return maxLength
-    return maxLength;
+//     Return nums.length-windStart
+    return nums.length - windowStart;
 };
