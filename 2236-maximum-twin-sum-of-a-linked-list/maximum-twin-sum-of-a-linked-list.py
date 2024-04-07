eaa49f969e21cdfaa10e5d6d7405d956480048eb
Nicholas Orsi <138147970+nickorsi@users.noreply.github.com>
2024-04-07 14:15:23 -0700

    Time: 368 ms (28.63%) | Memory: 44.6 MB (31.27%) - LeetSync

---
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.py
@@ -0,0 +1,47 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def pairSum(self, head: Optional[ListNode]) -> int:
+#   Find middle of LL
+        half_ll_length = 0
+        previous_orig = None
+        slow = head
+        fast = head.next.next
+
+        while fast and fast.next:
+            half_ll_length += 1
+            previous_orig = slow
+            slow = slow.next
+            fast = fast.next.next
+
+#   Reverse second half of LL
+        current = slow.next
+        previous_new = None
+
+        while current:
+            next_node = current.next
+            current.next = previous_new
+            previous_new = current
+            current = next_node
+    
+# Reassign middle of LL to the reversed portion of the LL
+        slow.next = previous_new
+#   Iterate through LL with two pointers, one at head and other at n+1/2
+        pointer2 = head
+        for position in range(half_ll_length):
+            pointer2 = pointer2.next
+
+        pointer1 = head
+        pointer2 = pointer2.next
+        max_sum = float('-inf')
+
+        while pointer2:
+            max_sum = max(max_sum, pointer1.val + pointer2.val)
+            pointer1 = pointer1.next
+            pointer2 = pointer2.next
+
+        return max_sum
+
