3ace6561eab5758888ecf09f19f99bfd98da6f09
Nicholas Orsi <138147970+nickorsi@users.noreply.github.com>
2024-04-15 18:39:44 -0700

    Time: 40 ms (57.45%) | Memory: 18.1 MB (29.44%) - LeetSync

--- 270-closest-binary-search-tree-value/closest-binary-search-tree-value.py
+++ 270-closest-binary-search-tree-value/closest-binary-search-tree-value.py
@@ -6,19 +6,37 @@
 #         self.right = right
 class Solution:
     def closestValue(self, root: Optional[TreeNode], target: float) -> int:
-        def helper(root: Optional[TreeNode], target: float, closest_value: int) -> int:
-            if not root: 
-                return closest_value
+#         def helper(root: Optional[TreeNode], target: float, closest_value: int) -> int:
+#             if not root: 
+#                 return closest_value
             
-            if abs(root.val - target) < abs(closest_value - target):
-                closest_value = root.val
-            if abs(root.val - target) == abs(closest_value - target):
-                closest_value = closest_value if closest_value < root.val else root.val
+#             if abs(root.val - target) < abs(closest_value - target):
+#                 closest_value = root.val
+#             if abs(root.val - target) == abs(closest_value - target):
+#                 closest_value = closest_value if closest_value < root.val else root.val
 
-            if target > root.val:
-                return helper(root.right, target, closest_value)
+#             if target > root.val:
+#                 return helper(root.right, target, closest_value)
             
-            else:
-                return helper(root.left, target, closest_value)
+#             else:
+#                 return helper(root.left, target, closest_value)
             
-        return helper(root, target, float('inf'))
+#         return helper(root, target, float('inf'))
+        def sort_nodes(root: Optional[TreeNode]) -> List[int]:
+            if not root: return []
+            sorted_nodes =  [*sort_nodes(root.left), root.val, *sort_nodes(root.right)]
+            return sorted_nodes
+        
+        sorted_values = sort_nodes(root)
+    
+        min_val = float('inf')
+        
+        for val in sorted_values:
+            if abs(min_val - target) > abs(val - target):
+                min_val = val
+        
+        return min_val
+           
+            
+        
+        
