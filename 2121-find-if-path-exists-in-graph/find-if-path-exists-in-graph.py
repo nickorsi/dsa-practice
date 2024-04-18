859a30bbf0f0b5006b7a548f707cac006c6578f3
Nicholas Orsi <138147970+nickorsi@users.noreply.github.com>
2024-04-17 20:33:28 -0700

    Time: 1549 ms (58.14%) | Memory: 107.4 MB (85.78%) - LeetSync

---
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -0,0 +1,36 @@
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+#       If source and destination are the same, return true
+        if source == destination: return True
+#       Will develop a hash map of nodes to neighbors
+        nodes_to_neighbors: {int: List[int]} = {}
+        
+        for node1, node2 in edges:
+            if node1 in nodes_to_neighbors:
+                nodes_to_neighbors[node1].append(node2)
+            else:
+                nodes_to_neighbors[node1] = [node2]
+            if node2 in nodes_to_neighbors:
+                nodes_to_neighbors[node2].append(node1)
+            else:
+                nodes_to_neighbors[node2] = [node1]
+#       Then do DFS traversal starting at node source to see if destination is ever reached while avoiding viewing an repeated nodes
+
+        seen = set()
+        valid_path = False
+        def dfs(n: int):
+            if destination in nodes_to_neighbors[n]: 
+                nonlocal valid_path
+                valid_path = True
+                return
+            if n not in seen:
+                seen.add(n)
+                for neighbor in nodes_to_neighbors[n]:
+                    dfs(neighbor)
+        
+        dfs(source)
+        
+        return valid_path
+                
+        
+                
