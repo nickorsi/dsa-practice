17c165caa27381b1b91aea01bfd60c7785596649
Nicholas Orsi <138147970+nickorsi@users.noreply.github.com>
2024-06-19 22:44:05 -0700

    Time: 91 ms (91.71%) | Memory: 61.6 MB (88.51%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -6,22 +6,22 @@ var productExceptSelf = function(nums) {
     // CANT USE '/'
     // MUST BE o(n) runtime
     // Build prefix going left and suffix going right
-    const preffix = new Array(nums.length).fill(1);
-    const suffix = new Array(nums.length).fill(1);
+    const ans = new Array(nums.length).fill(1);
 
     for(let i = 0; i < nums.length; i++) {
         if(nums[i-1] !== undefined) {
-            preffix[i] = preffix[i-1] * nums[i-1];
+            ans[i] = ans[i-1] * nums[i-1];
         }
     }
+
+    let accum = 1;
 
     for(let i = nums.length - 1; i >= 0 ; i--) {
         if(nums[i+1] !== undefined) {
-            suffix[i] = suffix[i+1] * nums[i+1];
+            accum *= nums[i+1]
         }
+            ans[i] = ans[i] * accum;
     }
-
-    return preffix.map((el, i, arr) => {
-        return el * suffix[i];
-    })
+    
+    return ans
 };
