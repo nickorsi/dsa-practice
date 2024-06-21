7fe84fede5d086aafd6a6802168db7e853688b4d
Nicholas Orsi <138147970+nickorsi@users.noreply.github.com>
2024-06-20 17:19:38 -0700

    Time: 59 ms (90.55%) | Memory: 55 MB (10.28%) - LeetSync

--- 443-string-compression/string-compression.ts
+++ 443-string-compression/string-compression.ts
@@ -9,22 +9,12 @@ function compress(chars: string[]): number {
         if(s === '') {
             s += chars[i];
             count ++;
-        // } else if(i === chars.length - 1) {
-        //     if(count >= 1) {
-        //         count++;
-        //         s += count;
-        //         continue
-        //     }
-        //     s += chars[i];
-        //     continue
     // If chars[i] not equal to the last string
         } else if(chars[i] !== s[s.length-1]) {
     //  If count is greater than 1
             if(count > 1) {
     //      push in count
-                console.log('here', s);
                 s += count;
-                console.log(s);
             }
     //   push in letter
             s += chars[i];
@@ -32,16 +22,13 @@ function compress(chars: string[]): number {
             count = 1;
     // else increment count
         } else {
-            console.log('here')
             count ++;
             if(i === chars.length -1) {
                 s += count;
             }
         }
     } 
-    // for(let i = 0; i < s.length; i++) {
-    //     chars[i] = s[i];
-    // }
+
     chars.splice(0, chars.length, ...s);
 
     return chars.length;
