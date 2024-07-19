2cb245287b9380c82eebb26cfc82da9a1ec24ef0
Nicholas Orsi <138147970+nickorsi@users.noreply.github.com>
2024-07-19 16:44:30 -0700

    Time: 199 ms (11.28%) | Memory: 59 MB (56.41%) - LeetSync

---
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.ts
@@ -0,0 +1,41 @@
+function closeStrings(word1: string, word2: string): boolean {
+    // If lengths aren't the same, return false
+    if(word1.length !== word2.length) return false;
+    // Build frequency counter of the two strings, if they don't have the same count return false
+    const freqCount1: Record<string, number> = {};
+    const freqCount2: Record<string, number> = {};
+
+    for(let i = 0; i < word1.length; i ++) {
+        if(word1[i] in freqCount1) {
+            freqCount1[word1[i]] ++;
+        } 
+        if(!(word1[i] in freqCount1)) {
+            freqCount1[word1[i]] = 1;
+        }
+        if(word2[i] in freqCount2) {
+            freqCount2[word2[i]] ++;
+        }
+        if(!(word2[i] in freqCount2)) {
+            freqCount2[word2[i]] = 1;
+        }
+    }
+    
+    console.log(freqCount1, freqCount2);
+
+    for(const key in freqCount1) {
+        if(!(key in freqCount2)) return false;
+    }
+
+    const counts1 = Object.values(freqCount1).sort((a,b) => a-b);
+    const counts2 = Object.values(freqCount2).sort((a,b) => a-b);
+
+    console.log(counts1, counts2);
+
+    for(let i = 0; i < counts1.length; i ++) {
+        if(counts1[i] !== counts2[i]) {
+            return false;
+        }
+    }
+
+    return true;
+};
