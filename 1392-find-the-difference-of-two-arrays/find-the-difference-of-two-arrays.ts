c9b32ce7b125271b043d167c74869dba63256cce
Nicholas Orsi <138147970+nickorsi@users.noreply.github.com>
2024-07-26 11:27:15 -0700

    Time: 87 ms (56.23%) | Memory: 57.7 MB (19.63%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.ts
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.ts
@@ -1,22 +1,13 @@
 function findDifference(nums1: number[], nums2: number[]): number[][] {
-    let ans: number[][] = [[],[]];
-    // Create hash set of nums1 and nums2
-    const set1 = new Set(nums1);
-    const set2 = new Set(nums2);
-    // Iterate through set1
-    for(const uniqueNum of set1) {
-        // If not in set2, push into ans[0]
-        if(!set2.has(uniqueNum)) {
-            ans[0].push(uniqueNum);
-        }
-        // Else remove from set2
-        else {
-            set2.delete(uniqueNum);
+    const set1: Set<number> = new Set(nums1);
+    const set2: Set<number> = new Set(nums2);
+    
+    for(const num of set1) {
+        if(set2.has(num)) {
+            set1.delete(num);
+            set2.delete(num);
         }
     }
-    // Assign ans[1] to spread out array of set2
-    ans[1] = [...set2];
-    // Return ans  
-    return ans;
 
+    return [[...set1], [...set2]];
 };
