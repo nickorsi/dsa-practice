48f4869864602c8222d59392a382f6e32dc9e5ab
Nicholas Orsi <138147970+nickorsi@users.noreply.github.com>
2024-07-26 11:59:46 -0700

    Time: 82 ms (6.94%) | Memory: 53.2 MB (28.10%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.ts
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.ts
@@ -13,11 +13,29 @@ function luckyNumbers (matrix: number[][]): number[] {
             if (matrix[row][col] > maxCol[col]) maxCol[col] = matrix[row][col];
         }
     }
-    // Iterate through minRow
-    for(const num of minRow) {
-        // If in maxCol push into luckyNums
-        if(maxCol.includes(num)) luckyNums.push(num);
-    }
-    // Return luckyNums
-    return luckyNums;
+    // // Iterate through minRow
+    // for(const num of minRow) {
+    //     // If in maxCol push into luckyNums
+    //     if(maxCol.includes(num)) luckyNums.push(num);
+    // }
+    // // Return luckyNums
+    // return luckyNums;
+    // Above it O(n*m) can it be done better? Have to traverse the whole matrix, BUT can do this greedily knowing that only 1 luckyNum can exist (see the editorial for explanation).
+    // Want the lowest colMax and highest rowMin, and if they aren't the same then no lucky num
+    // const minRow: number | undefined;
+    // const maxCol: number | undefined;
+    // // Iterate through row m
+    //  for(let row = 0; row < matrix.length; row ++) {
+    //     // Iterate through col n
+    //     for(let col = 0; col < matrix[0].length; col ++) {
+    //         // If value at matrix[m][n] is smaller than minRow[m] then replace
+    //         if (matrix[row][col] < minRow[row]) minRow[row] = matrix[row][col];
+    //         // If value at matrix[m][n] is larger than maxcol[n] then replace
+    //         if (matrix[row][col] > maxCol[col]) maxCol[col] = matrix[row][col];
+    //     }
+    // }
+    const maxMinRow = Math.max(...minRow);
+    const minMaxCol = Math.min(...maxCol);
+    
+    return maxMinRow === minMaxCol ? [maxMinRow] : [];
 };
