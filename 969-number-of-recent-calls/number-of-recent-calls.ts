1858fb1dab6d998ef803519c890836e37a08334c
Nicholas Orsi <138147970+nickorsi@users.noreply.github.com>
2024-07-27 18:44:49 -0700

    Time: 344 ms (30.74%) | Memory: 64.8 MB (27.66%) - LeetSync

---
+++ 969-number-of-recent-calls/number-of-recent-calls.ts
@@ -0,0 +1,23 @@
+class RecentCounter {
+    set: Set<number>;
+
+    constructor() {
+        this.set = new Set();    
+    }
+
+    ping(t: number): number {
+        this.set.add(t);
+        for(const storedT of this.set) {
+            if (storedT < t - 3000) {
+                this.set.delete(storedT)
+            }
+        }
+        return this.set.size;
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * var obj = new RecentCounter()
+ * var param_1 = obj.ping(t)
+ */
