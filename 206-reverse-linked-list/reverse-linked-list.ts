561fb1b57ff0762d9fd524330670eda7f15512d1
Nicholas Orsi <138147970+nickorsi@users.noreply.github.com>
2024-09-05 16:18:46 -0700

    Time: 62 ms (68.19%) | Memory: 52.7 MB (46.85%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.ts
+++ 206-reverse-linked-list/reverse-linked-list.ts
@@ -11,35 +11,6 @@
  */
 
 function reverseList(head: ListNode | null): ListNode | null {
-    // if (head === null) return head;
-    // // Iterate through to get the tail and count
-    // let currentNode = head;
-    // let count = 1;
-    // while(currentNode.next !== null) {
-    //     count ++;
-    //     currentNode = currentNode.next;
-    // }
-
-    // if (count < 2) return head;
-
-    // let tailNode = currentNode;
-    // let nextNode = head.next;
-    // currentNode = head;
-
-    // // Iterate through again assigning the currentNode to the tail until count is zero
-    // while(count !== 1) {
-    //     console.log(currentNode, nextNode, tailNode);
-    //     currentNode.next = null;
-    //     tailNode.next = currentNode;
-    //     currentNode = nextNode;
-    //     nextNode = currentNode.next;
-    //     console.log(currentNode, nextNode, tailNode);
-    //     tailNode = tailNode.next;
-    //     // console.log(currentNode, tailNode)
-    //     count --;
-    // }
-    // // Return the LL
-    // return currentNode;
     let prevNode = null;
     let currentNode = head;
 
