cf5276f844218ef79d29884e500780336bd25954
Nicholas Orsi <138147970+nickorsi@users.noreply.github.com>
2024-09-05 17:05:53 -0700

    Time: 405 ms (10.66%) | Memory: 44.6 MB (16.16%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.py
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.py
@@ -1,3 +1,4 @@
+from collections import deque 
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -5,43 +6,63 @@
 #         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-#   Find middle of LL
-        half_ll_length = 0
-        previous_orig = None
-        slow = head
-        fast = head.next.next
+# #   Find middle of LL
+#         half_ll_length = 0
+#         previous_orig = None
+#         slow = head
+#         fast = head.next.next
 
-        while fast and fast.next:
-            half_ll_length += 1
-            previous_orig = slow
-            slow = slow.next
-            fast = fast.next.next
+#         while fast and fast.next:
+#             half_ll_length += 1
+#             previous_orig = slow
+#             slow = slow.next
+#             fast = fast.next.next
 
-#   Reverse second half of LL
-        current = slow.next
-        previous_new = None
+# #   Reverse second half of LL
+#         current = slow.next
+#         previous_new = None
 
-        while current:
-            next_node = current.next
-            current.next = previous_new
-            previous_new = current
-            current = next_node
+#         while current:
+#             next_node = current.next
+#             current.next = previous_new
+#             previous_new = current
+#             current = next_node
     
-# Reassign middle of LL to the reversed portion of the LL
-        slow.next = previous_new
-#   Iterate through LL with two pointers, one at head and other at n+1/2
-        pointer2 = head
-        for position in range(half_ll_length):
-            pointer2 = pointer2.next
+# # Reassign middle of LL to the reversed portion of the LL
+#         slow.next = previous_new
+# #   Iterate through LL with two pointers, one at head and other at n+1/2
+#         pointer2 = head
+#         for position in range(half_ll_length):
+#             pointer2 = pointer2.next
 
-        pointer1 = head
-        pointer2 = pointer2.next
-        max_sum = float('-inf')
+#         pointer1 = head
+#         pointer2 = pointer2.next
+#         max_sum = float('-inf')
 
-        while pointer2:
-            max_sum = max(max_sum, pointer1.val + pointer2.val)
-            pointer1 = pointer1.next
-            pointer2 = pointer2.next
+#         while pointer2:
+#             max_sum = max(max_sum, pointer1.val + pointer2.val)
+#             pointer1 = pointer1.next
+#             pointer2 = pointer2.next
 
-        return max_sum
+#         return max_sum
 
+# Another optional solution
+# Iterate through entire LL and add vale to both a stack and queue while also getting count of the LL
+        queue = deque()
+        stack = []
+        count = 0
+        current_node = head
+
+        while current_node != None:
+            count += 1
+            stack.append(current_node.val)
+            queue.append(current_node.val)
+            current_node = current_node.next
+        # print(count, int(count/2))
+        max_count = float('-inf')
+        for _ in range(int(count/2)):
+            max_count = max(max_count, stack.pop() + queue.popleft())
+
+        return max_count
+# Then iterate through half the count, summing the stack and queue values and keeping track of the max
+# Return the max
