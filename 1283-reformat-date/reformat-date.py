81ee2e2ef4a3493950728bd68f4e41a512ec4f02
Nicholas Orsi <138147970+nickorsi@users.noreply.github.com>
2024-09-14 14:40:33 -0700

    Time: 35 ms (54.36%) | Memory: 16.8 MB (25.66%) - LeetSync

--- 1283-reformat-date/reformat-date.py
+++ 1283-reformat-date/reformat-date.py
@@ -1,15 +1,17 @@
 import datetime
 class Solution:
     def reformatDate(self, date: str) -> str:
-        date_list = date.split(" ")
-        day_list = list(date_list[0])
-        if len(day_list) == 4:
-            date_list[0] = day_list[0] + day_list[1]
-        else:
-            date_list[0] = "0" + day_list[0]
+        # date_list = date.split(" ")
+        # day_list = list(date_list[0])
+        # if len(day_list) == 4:
+        #     date_list[0] = day_list[0] + day_list[1]
+        # else:
+        #     date_list[0] = day_list[0]
 
-        formatted_date = " ".join(date_list)
+        # formatted_date = " ".join(date_list)
 
-        date_obj = datetime.datetime.strptime(formatted_date, "%d %b %Y")
+        # date_obj = datetime.datetime.strptime(formatted_date, "%d %b %Y")
+
+        date_obj = datetime.datetime.strptime(re.sub('st|nd|rd|th', '', date), "%d %b %Y")
 
         return date_obj.strftime("%Y-%m-%d")
