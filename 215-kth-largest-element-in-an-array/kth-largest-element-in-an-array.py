5e4126ed41a9ae0bae3f08d6e71fb6121545cb37
Nicholas Orsi <138147970+nickorsi@users.noreply.github.com>
2024-11-11 15:45:01 -0800

    Time: 93 ms (35.19%) | Memory: 27.5 MB (94.85%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -26,11 +26,11 @@ class Solution:
 # Can multiple by -1 to get max heaq
     def findKthLargest(self, nums: List[int], k: int) -> int:
         neg_nums = [el * -1 for el in nums]
-        print(neg_nums)
-        print(type(neg_nums))
+        # print(neg_nums)
+        # print(type(neg_nums))
         
         heapq.heapify(neg_nums)
-        print(neg_nums)
+        # print(neg_nums)
 
         for i in range(k - 1):
             heapq.heappop(neg_nums)
