b01416f1c39ebd26cc1ca811b962d46e367f4794
Nicholas Orsi <138147970+nickorsi@users.noreply.github.com>
2024-11-19 17:07:19 -0800

    Time: 1 ms (6.45%) | Memory: 17.6 MB (6.88%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -0,0 +1,20 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m, n = len(matrix), len(matrix[0]) # m is ROW n is COL
+        left = 0
+        right = m * n - 1
+
+        while left <= right:
+            mid = (right + left) // 2
+            # How to get row and col from mid? 
+            row = mid // n
+            col = mid % n 
+            print(m, n)
+            print(left, right, mid, row, col)
+            if matrix[row][col] == target: return True
+            if matrix[row][col] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        return False 
