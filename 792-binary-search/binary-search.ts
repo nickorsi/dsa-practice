b5e09fe4d89b1078df8adc08d8c9872e0d1ababd
Nicholas Orsi <138147970+nickorsi@users.noreply.github.com>
2024-11-19 16:35:07 -0800

    Time: 6 ms (5.24%) | Memory: 53.6 MB (53.88%) - LeetSync

---
+++ 792-binary-search/binary-search.ts
@@ -0,0 +1,18 @@
+function search(nums: number[], target: number): number {
+    let left = 0; 
+    let right = nums.length - 1;
+
+    while(left <= right) {
+        const mid = Math.floor((right + left) / 2);
+        console.log(left, right, mid)
+
+        if(nums[mid] === target) return mid;
+        else if(nums[mid] > target) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return -1;
+};
