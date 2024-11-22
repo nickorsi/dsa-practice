a4c90d1423f01e4c70e6e13055d56734be01de1c
Nicholas Orsi <138147970+nickorsi@users.noreply.github.com>
2024-11-21 21:48:00 -0800

    Time: 936 ms (86.91%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -3,5 +3,5 @@ SELECT e.name, b.bonus
     FROM Employee as e
     LEFT JOIN Bonus as b
         ON e.empId = b.empId
-    -- WHERE b.bonus < 1000 OR b.bonus IS NULL;
-    WHERE coalesce(b.bonus, 0) < 1000;
+    WHERE b.bonus < 1000 OR b.bonus IS NULL;
+    -- WHERE coalesce(b.bonus, 0) < 1000;
