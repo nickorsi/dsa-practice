f58b0501d3412b1e4585a3079925fcb61f1782d9
Nicholas Orsi <138147970+nickorsi@users.noreply.github.com>
2024-11-21 21:36:09 -0800

    Time: 566 ms (57.03%) | Memory: 0B (100.00%) - LeetSync

--- 1180-game-play-analysis-ii/game-play-analysis-ii.sql
+++ 1180-game-play-analysis-ii/game-play-analysis-ii.sql
@@ -1,8 +1,25 @@
--- Write your PostgreSQL query statement below
-WITH first_device_login as 
-    (SELECT player_id, device_id, ROW_NUMBER() OVER(PARTITION BY player_id ORDER BY event_date asc) as row_number  
-    FROM Activity)
-
+# Write your MySQL query statement below
 SELECT player_id, device_id
-    FROM first_device_login
-    WHERE row_number = 1;
+    FROM Activity
+    WHERE (player_id, event_date) in 
+        (SELECT player_id, min(event_date)
+            FROM Activity
+            GROUP BY player_id);
+
+-- WITH first_device_login as (
+--     SELECT 
+--     player_id, 
+--     device_id, 
+--     RANK() OVER(
+--         PARTITION BY 
+--             player_id 
+--             ORDER BY 
+--             event_date asc
+--     ) AS rnk 
+--     FROM 
+--         Activity
+-- )
+
+-- SELECT player_id, device_id
+--     FROM first_device_login
+--     WHERE rnk = 1;
