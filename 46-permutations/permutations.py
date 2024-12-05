53a71a33c26c17b277aabf3c1ee3158605aa21a6
Nicholas Orsi <138147970+nickorsi@users.noreply.github.com>
2024-12-04 17:53:51 -0800

    Time: 3 ms (36.37%) | Memory: 17.6 MB (6.57%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -21,7 +21,16 @@ class Solution:
 
         def backtrack(curr):
             if len(curr) == len(nums):
-                ans.append(curr[:])
+                ans.append(curr[:]) 
+                """
+                    Note how we are appending a copy of curr and not curr itself. This is becuase using curr directly 
+                    will only be passed as a reference and not as a value. This means appending curr will always point to
+                    the value that is assigned to curr. As this recursion progresses, the value assigned to curr will always 
+                    revert back to being an empty array [] based on how this function is implemented, where values are 
+                    appended but then ecentually popped off. If you were to try and append curr instead, you'll see the answer 
+                    will have the correct number of arrays in it, but they are all empty. So we have to append a copy of of 
+                    the value assigned to curr AT THIS SPECIFIC MOMENT when it needs to be appended to ans.
+                """ 
                 return
 
             for num in nums:
