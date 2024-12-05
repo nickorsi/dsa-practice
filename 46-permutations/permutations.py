b2d5790cf93e70d26efa30ed28cca9486f43e56a
Nicholas Orsi <138147970+nickorsi@users.noreply.github.com>
2024-12-04 18:01:21 -0800

    Time: 4 ms (13.81%) | Memory: 17.7 MB (6.57%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,46 +1,46 @@
 class Solution:
 
     def permute(self, nums: List[int]) -> List[List[int]]:
-        # permutations: List[int] = []
+        permutations: List[List[int]] = []
 
-        # def backtrack(nums: List[int], permutation: List[int]) -> None:
-        #     if len(nums) == 0:
-        #         permutations.append(permutation)
-        #         return
+        def backtrack(nums: List[int], permutation: List[int]) -> None:
+            if len(nums) == 0:
+                permutations.append(permutation)
+                return
             
-        #     for i in range(len(nums)):
-        #         new_permutation = [*permutation, nums[i]]
-        #         new_nums = [*nums[0:i:], *nums[i+1::]]
-        #         backtrack(new_nums, new_permutation)
+            for i in range(len(nums)):
+                new_permutation = [*permutation, nums[i]]
+                new_nums = [*nums[0:i:], *nums[i+1::]]
+                backtrack(new_nums, new_permutation)
         
-        # backtrack(nums, [])
+        backtrack(nums, [])
         
-        # return permutations
+        return permutations
 
         # Above solution does not take advantage of the fact that all nums are unique, can therefore do the below: 
 
-        def backtrack(curr):
-            if len(curr) == len(nums):
-                ans.append(curr[:]) 
-                """
-                    Note how we are appending a copy of curr and not curr itself. This is becuase using curr directly 
-                    will only be passed as a reference and not as a value. This means appending curr will always point to
-                    the value that is assigned to curr. As this recursion progresses, the value assigned to curr will always 
-                    revert back to being an empty array [] based on how this function is implemented, where values are 
-                    appended but then ecentually popped off. If you were to try and append curr instead, you'll see the answer 
-                    will have the correct number of arrays in it, but they are all empty. So we have to append a copy of of 
-                    the value assigned to curr AT THIS SPECIFIC MOMENT when it needs to be appended to ans.
-                """ 
-                return
+        # def backtrack(curr):
+        #     if len(curr) == len(nums):
+        #         ans.append(curr[:]) 
+        #         """
+        #             Note how we are appending a copy of curr and not curr itself. This is becuase using curr directly 
+        #             will only be passed as a reference and not as a value. This means appending curr will always point to
+        #             the value that is assigned to curr. As this recursion progresses, the value assigned to curr will always 
+        #             revert back to being an empty array [] based on how this function is implemented, where values are 
+        #             appended but then ecentually popped off. If you were to try and append curr instead, you'll see the answer 
+        #             will have the correct number of arrays in it, but they are all empty. So we have to append a copy of of 
+        #             the value assigned to curr AT THIS SPECIFIC MOMENT when it needs to be appended to ans.
+        #         """ 
+        #         return
 
-            for num in nums:
-                if num not in curr:
-                    curr.append(num)
-                    backtrack(curr)
-                    curr.pop()
+        #     for num in nums:
+        #         if num not in curr:
+        #             curr.append(num)
+        #             backtrack(curr)
+        #             curr.pop()
 
-        ans = []
-        backtrack([])
-        return ans
+        # ans = []
+        # backtrack([])
+        # return ans
     
             
