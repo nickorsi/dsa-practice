11765ed08df0ca32655da8d230495925a7f0a1a2
Nicholas Orsi <138147970+nickorsi@users.noreply.github.com>
2024-12-04 17:38:14 -0800

    Time: 0 ms (100.00%) | Memory: 17.5 MB (6.57%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,22 +1,37 @@
 class Solution:
 
     def permute(self, nums: List[int]) -> List[List[int]]:
-        permutations: List[int] = []
+        # permutations: List[int] = []
 
-        def backtrack(nums: List[int], permutation: List[int]) -> None:
-            if len(nums) == 0:
-                permutations.append(permutation)
-                return
+        # def backtrack(nums: List[int], permutation: List[int]) -> None:
+        #     if len(nums) == 0:
+        #         permutations.append(permutation)
+        #         return
             
-            for i in range(len(nums)):
-                new_permutation = [*permutation, nums[i]]
-                new_nums = [*nums[0:i:], *nums[i+1::]]
-                backtrack(new_nums, new_permutation)
+        #     for i in range(len(nums)):
+        #         new_permutation = [*permutation, nums[i]]
+        #         new_nums = [*nums[0:i:], *nums[i+1::]]
+        #         backtrack(new_nums, new_permutation)
         
-        backtrack(nums, [])
+        # backtrack(nums, [])
         
-        return permutations
+        # return permutations
 
+        # Above solution does not take advantage of the fact that all nums are unique, can therefore do the below: 
 
+        def backtrack(curr):
+            if len(curr) == len(nums):
+                ans.append(curr[:])
+                return
+
+            for num in nums:
+                if num not in curr:
+                    curr.append(num)
+                    backtrack(curr)
+                    curr.pop()
+
+        ans = []
+        backtrack([])
+        return ans
     
             
