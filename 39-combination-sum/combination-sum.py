31c6041ef565bebc4c9546e0ed6bf6cd9fcb29d7
Nicholas Orsi <138147970+nickorsi@users.noreply.github.com>
2024-12-09 17:00:14 -0800

    Time: 12 ms (42.84%) | Memory: 17.4 MB (13.89%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -2,7 +2,26 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         combinations: List[int] = []
 
-        def backtrack(curr_combo: List[int], curr_sum:int, curr_int: int) -> None:
+        # def backtrack1(curr_combo: List[int], curr_sum:int, curr_int: int) -> None:
+        #     if curr_sum >= target:
+        #         if curr_sum == target:
+        #             combinations.append(curr_combo[:])
+        #         return
+            
+        #     for i in range(curr_int, len(candidates)):
+        #         curr_combo.append(candidates[i])
+        #         curr_sum += candidates[i]
+        #         backtrack(curr_combo, curr_sum, i)
+        #         curr_combo.pop()
+        #         curr_sum -= candidates[i]
+        
+        #     return
+
+        # backtrack1([], 0, 0)
+
+        def backtrack2(curr_combo: List[int], curr_int: int) -> None:
+            curr_sum = sum(curr_combo)
+
             if curr_sum >= target:
                 if curr_sum == target:
                     combinations.append(curr_combo[:])
@@ -10,12 +29,12 @@ class Solution:
             
             for i in range(curr_int, len(candidates)):
                 curr_combo.append(candidates[i])
-                curr_sum += candidates[i]
-                backtrack(curr_combo, curr_sum, i)
+                # curr_sum += candidates[i]
+                backtrack2(curr_combo, i)
                 curr_combo.pop()
-                curr_sum -= candidates[i]
+                # curr_sum -= candidates[i]
         
             return
         
-        backtrack([], 0, 0)
+        backtrack2([], 0)
         return combinations
