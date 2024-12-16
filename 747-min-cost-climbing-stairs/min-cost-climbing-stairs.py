1f4fff7f2d1068f82c32042e1290568738b01e54
Nicholas Orsi <138147970+nickorsi@users.noreply.github.com>
2024-12-16 13:07:07 -0800

    Time: 3 ms (73.22%) | Memory: 17.3 MB (38.78%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,17 +1,26 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         n: int = len(cost)
-        memo: dict[int, int] = {}
-        def dp(i: int) -> int:
-            # Base case
-            if i <= 1:
-                return 0
+        # Top Down DP
+        # memo: dict[int, int] = {}
+        # def dp(i: int) -> int:
+        #     # Base case
+        #     if i <= 1:
+        #         return 0
             
-            if i in memo: 
-                return memo[i]
+        #     if i in memo: 
+        #         return memo[i]
             
-            memo[i] = min(dp(i - 1) + cost[i - 1], dp(i - 2) + cost[i - 2])
+        #     memo[i] = min(dp(i - 1) + cost[i - 1], dp(i - 2) + cost[i - 2])
 
-            return memo[i]
+        #     return memo[i]
         
-        return dp(n)
+        # return dp(n)
+
+        # Bottom Up DP
+        dp = [0] * (n + 1)
+
+        for i in range(2, n + 1):
+            dp[i] = min(dp[i - 1] + cost [i - 1], dp[i - 2] + cost [i - 2])
+
+        return dp[-1]
