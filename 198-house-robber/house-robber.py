5105fc53d55cd55b06c638cf87dc3e0245dad957
Nicholas Orsi <138147970+nickorsi@users.noreply.github.com>
2024-12-17 14:55:39 -0800

    Time: 0 ms (100.00%) | Memory: 17.6 MB (6.06%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -44,13 +44,27 @@ class Solution:
         # return dp(n)
 
         # Option 3: Use cache decorator to automatically memoize the function
-        @cache
-        def dp(i: int) -> int:
-            if i == 0:
-                return nums[i]
-            if i == 1:
-                return max(nums[0], nums[i])
+        # @cache
+        # def dp(i: int) -> int:
+        #     if i == 0:
+        #         return nums[i]
+        #     if i == 1:
+        #         return max(nums[0], nums[i])
             
-            return max(nums[i] + dp(i - 2), dp(i - 1))
+        #     return max(nums[i] + dp(i - 2), dp(i - 1))
         
-        return dp(n)
+        # return dp(n)
+
+        # Option 4: Use top down dp
+
+        dp: List[int] = [0] * len(nums)
+
+        for i in range(len(nums)):
+            if i == 0:
+                dp[i] = nums[i]
+            elif i == 1:
+                dp[i] = max(nums[0], nums[i])
+            else:
+                dp[i] = max(nums[i] + dp[i-2], dp[i - 1])
+
+        return dp[-1]
