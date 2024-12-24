11f8815eae0b966a563d354f5c2dd133c60d30a3
Nicholas Orsi <138147970+nickorsi@users.noreply.github.com>
2024-12-23 18:21:38 -0800

    Time: 0 ms (100.00%) | Memory: 18 MB (18.44%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -24,11 +24,24 @@ class Solution:
         # return dp(len(cost))
 
         # Bottom up
+        # n = len(cost)
+        # dp = [0] * (n + 1)
+
+        # for step in range(2, n + 1):
+        #     dp[step] = min(dp[step - 1] + cost[step - 1], dp[step - 2] + cost[step - 2])
+
+        # print(dp)
+        # return dp[-1]
+
+        # Bottom up only. caring about last 2 steps
         n = len(cost)
-        dp = [0] * (n + 1)
+        dp = [0] * 2
 
         for step in range(2, n + 1):
-            dp[step] = min(dp[step - 1] + cost[step - 1], dp[step - 2] + cost[step - 2])
+            step1, step2 = dp
 
+            dp[1] = min(step1 + cost[step - 2], step2 + cost[step - 1])
+            dp[0] = step2
+        
         print(dp)
-        return dp[-1]
+        return dp[1]
