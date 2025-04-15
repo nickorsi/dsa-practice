bba8c87ad5a6aba548bef41993d76a5d7b03581d
Nicholas Orsi <138147970+nickorsi@users.noreply.github.com>
2025-04-14 21:52:07 -0500

    Time: 7 ms (56.56%) | Memory: 19.4 MB (19.95%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,5 +1,19 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-
-        return nums[math.ceil((len(nums) - 1) / 2)]
+        # N log N time
+        # nums.sort()
+        # return nums[math.ceil((len(nums) - 1) / 2)]
+        # Linear time
+        # Iterate through tracking candidate and count
+        ans = 0
+        count = 0 
+        for num in nums: 
+            if count == 0:
+                ans = num
+                count += 1
+            elif num == ans:
+                count += 1
+            else: 
+                count -= 1
+        
+        return ans
